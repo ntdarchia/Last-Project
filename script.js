1af7119a29d1633dcd1b821f6cca2f23c3c64a61
natiadarchia <99994815+natiadarchia@users.noreply.github.com>
2022-04-30 01:42:55 +0400

    Start responsive

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-$(function(){
-    $('.multiple-items').slick({
-     infinite: true,
-     slidesToShow: 3,
-     slidesToScroll: 1,
-     autoplay : true,
-     autoplaySpeed: 2000,
-     arrows: true,
-     dots:true,
-     responsive: [
-         {
-           breakpoint: 768,
-           settings: {
-             slidesToShow: 2,
-             slidesToScroll: 1
-           }
-         },
-         {
-           breakpoint: 480,
-           settings: {
-             slidesToShow: 1,
-             slidesToScroll: 1
-           }
-         }
-     ]
-   });    
- });
+$(document).ready(function() {
+  var options = {
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay : false,
+    autoplaySpeed: 2000,
+    arrows: true,
+    dots:true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+    ]
+   };
+  $('.multiple-items').slick(options);
+});
+
+ //Add smooth scrolling to all links
+ $(document).ready(function () {
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        900,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+});
+
+// Sidebar
+
+
+	const menu_btn = document.querySelector('.hamburger');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+	});
+
+
+
